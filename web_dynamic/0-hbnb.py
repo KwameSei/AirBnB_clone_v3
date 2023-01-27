70f4b903418c06591d3fbf33b2afe1685d1928c7
Stephy <atienostephanie040@gmail.com>
2023-01-27 15:12:56 +0000

    base route removed

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -18,10 +18,6 @@ def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-@app.route('/')
-def home():
-    return "woe"
-
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
