bb9e4c729dca05043912fd9cb71b19e83e33440b
Stephy <atienostephanie040@gmail.com>
2023-01-27 13:40:05 +0000

    task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,6 +6,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+from uuid import uuid4
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -16,7 +17,6 @@ app = Flask(__name__)
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
-
 
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
@@ -37,7 +37,8 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
